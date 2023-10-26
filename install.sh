14802cb47b04ffc7adbde67856e2367927a6494a
Geonmo Ryu <geonmo@kisti.re.kr>
2023-10-26 06:07:32 +0000

    HTcondor 클러스터 구축 관련 업데이트

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd htcondorce_on_vagrant
-./00_install_role.sh
+#cd htcondorce_on_vagrant
+htcondorce_on_vagrant/00_install_role.sh
 
 ansible-playbook 01_hosts.yml
 ansible-playbook 02_epel.yml
+ansible-playbook 06_htcondor.yml
 
